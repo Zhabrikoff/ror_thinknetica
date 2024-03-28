0cda711411858a2f5802d69aa5f633b5758180bf
Zhabrikoff <ezhabrikov@gmail.com>
2024-03-28 22:49:52 +0300

    feat: add an answer to the task area of a triangle

---
+++ ruby_basics_part_1/area_of_a_triangle.rb
@@ -0,0 +1,8 @@
+puts "Enter the base of the triangle"
+base = gets.chomp.to_i
+
+puts "Enter the height of the triangle"
+heigth = gets.chomp.to_i
+
+area = 0.5 * base * heigth
+puts "The area of the triangle is #{area}"
