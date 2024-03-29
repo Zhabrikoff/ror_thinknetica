86934a0ba6a7fb26baa6b039b564d2329801343c
Zhabrikoff <ezhabrikov@gmail.com>
2024-03-29 22:23:22 +0300

    feat: add an answer to the task months

---
+++ ruby_basics_part_2/months.rb
@@ -0,0 +1,16 @@
+months = {
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31,
+}
+
+months.each { |month, amount_of_days| puts month if amount_of_days == 30 }
