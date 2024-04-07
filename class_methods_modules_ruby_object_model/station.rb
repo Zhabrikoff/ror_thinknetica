ba2e8a9717611670483a84b726999ff3112cbfe6
Zhabrikoff <ezhabrikov@gmail.com>
2024-04-07 18:17:29 +0300

    init: start task Class Methods, Modules, and the Ruby Object Model

---
+++ class_methods_modules_ruby_object_model/station.rb
@@ -0,0 +1,26 @@
+class Station
+  attr_reader :name, :trains
+
+  def initialize(name)
+    @name = name
+    @trains = []
+  end
+
+  def take_train(train)
+    trains << train
+  end
+
+  def send_train(train)
+    trains.delete(train)
+  end
+
+  def trains_by_type
+    count = Hash.new(0)
+
+    trains.each do |train|
+      count[train.type] += 1
+    end
+
+    count
+  end
+end
